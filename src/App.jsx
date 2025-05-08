86ffc9742a0b8f39f05ab0f82c7dcb91d78ffb0b
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-05-08 22:00:09 +0530

    React js

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,6 @@ const App = () => {
     <nav>
       <div>
         <a href="">Home</a>
-        <a href="">Service</a>
-        <a href="">Contact</a>
-        <a href="">Home</a>
-        <a href="">Home 2</a>
       </div>
     </nav>
   )
