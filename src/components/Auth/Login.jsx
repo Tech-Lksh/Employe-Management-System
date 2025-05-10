9e9c215d8d0e76aaa03c7694dd60246322dd97f0
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-05-10 18:15:08 +0530

    React js

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div className=''>
-      Login
+    <div className='flex h-screen w-screen items-center justify-center'>
+      <div className='border-2 border-red-600'>
+          <form className='flex flex-col items-center justify-center'>
+            <input className='border-2 border-emerald-600 py-3 px-5 rounded-full' type="email" placeholder='Enter your email'/>
+            <input type="password" placeholder='Enter your password'/>
+            <button>Log in</button>
+          </form>
+      </div>
     </div>
   )
 }
